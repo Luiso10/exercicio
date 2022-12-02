935c805964ee400396a236a99eef24cd8ff8fb42
Henryco10 <>
2022-12-02 01:14:58 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+class program {
+public static void main (string args){
+// declara variavel
+// var nome: caractere
+string nome;
+
+// leia(nome)  
+nome = console,readluis();  
+
+//escreva ("hello word")
+console.wrireluis ("hello" + nome);
+  
